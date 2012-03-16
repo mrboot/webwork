4e8e2ffc7a708836fe98c9d3a45324741a6fa662
Mark Booth <mrbooth@gmail.com>
2012-03-16 13:52:14 +0000

    Finish adding jQuery stuff
    Add more jQuery to the contact us page fields.
    Add a slideToggle to the images on the gallery to show and hide the captions.
    Fix an issue in the CSS to ensure just [type=text] input fields are lengthened.

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+
 	$("#name").val("Please enter your full name");
+	$("#field-55d5b2719737aea").val("Incl local dial code");
+	$("#field-3d71e928db259fd").val("Format DDMMYYYY");
+	$("#field-360c1692698b95b").val("The more you enter here, the less we have to check with you :-)");
+	$("input, textarea").focus(function(){
+		$(this).select();
+	});
+	$("figcaption").hide();
+	$("figure").each(function(){
+		$(this).hover(function(){
+			$(this).find("figcaption").slideToggle('slow');
+		});
+	});
+
 });

08ea1452a1bb87a6667afe05a2ccfe30cdcb1113
Mark Booth <mrbooth@gmail.com>
2012-04-20 13:45:34 +0100

    Style the input control. Rework the jQuery to use one long string and split it out to form the table data.

--- scripts/addTableRow.js
+++ scripts/addTableRow.js
@@ -3,30 +3,13 @@ $(document).ready(function (){
 	// Global variable to hold table row string
 	var rowString = "<tr><td>surName</td><td>foreNames</td><td>gender</td><td>dateOfBirth</td><td>expiryDate</td><td>docType</td><td>docNumber</td><td>issuingState</td><td>nationality</td></tr>"
 
-	// On receipt of data string, set focus to hidden field
+	// On receipt BEGIN_SWIPE_DATA, set focus to hidden field
 	// This is indicated by sending special character listened for by this function
 	// In testing this is set to "[" (ASCII code 91)
   $("body").keypress(
     function(event) {
       if (event.which == 91) {
         $("input#hiddenInput").focus();
-      };
-    }
-  );
-
-	// receive text from swipe into this text field...
-
-	// on completion of sending data receive special keypress
-	// In testing this is set to "]" (ASCII code 9)
-  $("body").keypress(
-    function(event) {
-      if (event.which == 93) {
-				// read contents of field and split data either side of ":" char
-				// store in array variable
-				var fieldData = $("input#hiddenInput").val().replace(/(\[|\])/g, '').split(":");
-
-				// update global variable to replace placeholder text with correct value
-				rowString = rowString.replace(fieldData[0], fieldData[1]);
 
         // clear the hidden field
         $("input#hiddenInput").val('');
@@ -34,19 +17,40 @@ $(document).ready(function (){
     }
   );
 
+	// receive text from swipe into this text field in comma delimtied form...
+
 	// On receipt of END_SWIPE_DATA use global variable to add row to table.
 	// This is indicated by sending special character listened for by this function
-	// In testing this is set to "=" (ASCII code 93)
+	// In testing this is set to "]" (ASCII code 93)
   $("body").keypress(
     function(event) {
-      if (event.which == 61) {
+      if (event.which == 93) {
+        // read contents of field and split into array based on "," char and
+        // removing the leading and trailing [] characters.
+        // store in array variable
+        var fieldData = $("input#hiddenInput").val().replace(/(\[|\])/g, '').split(",");
+
+        // Get rid of the last element in the array as its a blank entry
+        fieldData.pop();
+
+        // Iterate through the array splitting out each element into key value pairs.
+        // update global variable to replace placeholder text with correct value
+        // using the key value pairs
+        // rowString = rowString.replace(fieldData[0], fieldData[1]);
+        for (var i = 0; i < fieldData.length; i++) {
+          rowString = rowString.replace(fieldData[i].split(":")[0], 
+                                        fieldData[i].split(":")[1]);
+        };
+
+        // add a row after the current last row of the table.
         $('tr:last').after(rowString);
       };
     }
   );
 
-
 });
 
+// Test String for the text field
+// [surName:Booth,foreNames:Mark,gender:Male,dateOfBirth:18-12-1972,expiryDate:20-01-2014,docType:Passport,docNumber:123456789,issuingState:GBR,nationality:GBR,]
 
 
